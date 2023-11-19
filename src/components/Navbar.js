61ee4c19306e1554ac763513430b94b1c8bfe587
sahlminkok <sahlminkok7@gmail.com>
2023-11-19 18:51:03 +0100

    Add market and cart links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>Navbar</div>
+  <div className="navbar">
+    <Link to="/">Market</Link>
+    <Link to="/shopping-cart">Cart (1)</Link>
+  </div>
 );
 
 export default Navbar;
