280ad01f7f684ee8a5cf3d32ab7f92f6a64e2607
sahlminkok <sahlminkok7@gmail.com>
2023-11-19 16:39:16 +0100

    create products data object

---
+++ src/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    name: 'Barbie Puppy',
+    color: 'White',
+    image: 'baju1',
+    price: 4000,
+  },
+  {
+    name: 'Bloody Butterfly Zero Zero',
+    image: 'baju2',
+    color: 'Pink',
+    price: 5000,
+  },
+  {
+    name: 'Cactus Omoa',
+    color: 'White',
+    image: 'baju3',
+    price: 9000,
+  },
+];
+
+export default products;
