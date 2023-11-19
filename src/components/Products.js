a82afa5a5bf4de7afdcf5548700e3103acbedc0e
sahlminkok <sahlminkok7@gmail.com>
2023-11-19 18:48:45 +0100

    Import products data and display it on Products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import products from '../products';
 
 const Products = () => (
-  <div>Products</div>
+  <div className="products">
+    {
+      products.map((product) => (
+        <div className="product" key={product.name}>
+          <img src={product.image} alt={product.name} />
+          <p>
+            {product.name}
+            {' '}
+            -
+            {' '}
+            {product.color}
+          </p>
+          <div>
+            <p>
+              ₦
+              {product.price}
+            </p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+      ))
+    }
+  </div>
 );
 
 export default Products;
