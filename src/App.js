648749c0591b648d7acb8566ebd1646459e596d4
sahlminkok <sahlminkok7@gmail.com>
2023-11-19 18:54:19 +0100

    Create routes for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Market from './pages/Market';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Market />} />
+        <Route path="/shopping-cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
